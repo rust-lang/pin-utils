bc006b76d5fab7026ec34bb0b988bfe9975c13c1
Taiki Endo <te316e89@gmail.com>
2021-01-05 17:33:05 +0900

    Remove html_root_url

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 #![warn(missing_docs, missing_debug_implementations)]
 #![deny(bare_trait_objects)]
 #![allow(unknown_lints)]
-#![doc(html_root_url = "https://docs.rs/pin-utils/0.1.0")]
 
 #[macro_use]
 mod stack_pin;
